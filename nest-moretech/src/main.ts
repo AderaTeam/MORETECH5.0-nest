1ab6d6e61a6db71b1efd8e6f9a8cff06c31f6e94
Isachenko Ivan <vanisachenko@gmail.com>
2023-10-14 16:17:43 +0600

    CORS for https

--- nest-moretech/src/main.ts
+++ nest-moretech/src/main.ts
@@ -9,7 +9,11 @@ async function bootstrap() {
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       "http://178.170.192.87:3000",
-      "http://178.170.192.87:3000/"
+      "http://178.170.192.87:3000/",
+      "https://adera-team.ru",
+      "http://adera-team.ru",
+      "https://adera-team.ru/",
+      "http://adera-team.ru/",
     ]
   })
   await app.listen(8000);
