f2410298f96e79ae30d284499840660185dc3dbf
Isachenko Ivan <vanisachenko@gmail.com>
2023-10-15 11:46:56 +0600

    regex

--- python/app/cv.py
+++ python/app/cv.py
@@ -4,9 +4,8 @@ from random import choice
 import numpy as np 
 
 def load_image():
-    w = choice(glob.glob('.\\mock_img\\*.jpg'))
+    w = choice(glob.glob('./mock_img/*.jpg'))
     print(w)
     with PIL.Image.open(w) as im:
         x = im.resize((224, 224))
     return np.array(x)
-    
